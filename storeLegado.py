b0b85282cf45f5c991feb93d65afab8d79df3bf6
Gilles Lima <gillestoim@gmail.com>
2018-09-27 16:29:06 -0300

    update storeLegado.py

--- storeLegado.py
+++ storeLegado.py
@@ -9,10 +9,10 @@ import logging
 
 def run():
 
-    path_local = 'C:/xampp/htdocs/.../wordpress/wp-content/uploads/legado/'
-    path_http  = 'http://localhost/.../wordpress/wp-content/uploads/legado/'
+    path_local = 'C:/xampp/htdocs/desenvolvimento/SISAN-LEGADO/wordpress/wp-content/uploads/legado/'
+    path_http  = 'http://localhost/desenvolvimento/SISAN-LEGADO/wordpress/wp-content/uploads/legado/'
 
-    categories  = { 'Cultura': 2, 'Econômia': 3, 'Habitação': 3, 'Esporte': 4, 'Loterias': 5, 'Programas e Benefícios': 6 }
+    categories  = { 'Cultura': 2, 'Economia': 3, 'Habitação': 3, 'Esporte': 4, 'Loterias': 5, 'Programas e Benefícios': 6, 'Finanças': 3, 'Atendimento': 3 }
 
     regions = {
         'Centro-Oeste': 18, 'Distrito Federal': 302, 'Góias': 303, 'Mato Grosso': 304, 'Mato Grosso do Sul': 305, 'Nordeste': 15, 'Alagoas': 296, 'Bahia': 297, 'Ceará': 295, 'Maranhão': 301, 'Paraíba': 293, 'Pernambuco': 294, 'Piauí': 299, 'Rio Grande do Norte': 300, 'Sergipe': 298, 'Sudeste': 17, 'Espiríto Santo': 313, 'São Paulo': 316, 'Rio de Janeiro': 315, 'Minas Gerais': 314, 'Sul': 16, 'Paraná': 317, 'Santa Catarina': 318, 'Rio Grande do Sul': 319, 'Norte': 14, 'Acre': 306, 'Amapá': 307, 'Amazonas': 308, 'Pará': 309, 'Rondonia': 310, 'Roráima': 311, 'Tocantins': 31
@@ -23,7 +23,7 @@ def run():
         host='localhost',
         user='root',
         password='',
-        db='my_db'
+        db='sisanv2_db'
     )
 
     with open('manifest-formatado2.json', encoding='utf-8') as f:
@@ -71,6 +71,7 @@ def run():
                             insert_categories = "INSERT INTO wp_term_relationships(object_id, term_taxonomy_id, term_order) VALUES(%s, %s, %s)"
                             cursor.execute(insert_categories, (postid, reg, 0))
                         
+                        # in my case, is necessary a image download
                         if 'ImageHomeEditorial' in p: 
                             image    = str(p['ImageHomeEditorial']).split(', ')[0]
                             img_name = image[image.rindex('/')+1:]
@@ -80,30 +81,14 @@ def run():
                             url[2] = urllib.parse.quote(url[2],  safe='/', encoding='utf-8', errors='strict')
                             url = urllib.parse.urlunsplit(url)
                             print(url)
-                            
+
                             try:
-                                # try download image
+                                #try download image
                                 urllib.request.urlretrieve(url, path_local + img_name)
 
                             except urllib.error.URLError as e:
                                 logging.basicConfig(filename = 'log.txt', format = '%(asctime)s  %(levelname)-10s %(processName)s  %(name)s %(message)s')
                                 logging.error(e.reason)
-
-                            # insert reference of image into wordpress table 'wp_posts'. post_type = attachment
-                            insert_image = "INSERT INTO wp_posts(post_author, post_date, post_date_gmt, post_content, post_title, post_excerpt, post_status, comment_status, ping_status, post_password, post_name, to_ping, pinged, post_modified, post_modified_gmt, post_content_filtered, post_parent, guid, menu_order, post_type, post_mime_type, comment_count) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-
-                            cursor.execute(insert_image, (1, date, date, '', img_name.split('.')[0], '', 'inherit', 'open', 'closed', '', slug, '', '', date, date, '', postid, path_http + img_name, 0, 'attachment', '', 0))
-
-                            imageid = cursor.lastrowid
-                            print('Image ID %d' %(imageid))
-
-                            # insert into wordpress table 'wp_postmeta' the id of image
-                            insert_image_postmeta = "INSERT INTO wp_postmeta(post_id, meta_key, meta_value) VALUES(%s, %s, %s)"
-                            cursor.execute(insert_image_postmeta, (postid, '_thumbnail_id', imageid))
-
-                            # insert into wordpress table 'wp_postmeta' the path of image
-                            insert_image_postmeta = "INSERT INTO wp_postmeta(post_id, meta_key, meta_value) VALUES(%s, %s, %s)"
-                            cursor.execute(insert_image_postmeta, (imageid, '_wp_attached_file', 'legado/'+ img_name))
                         
                         conn.commit()
         
